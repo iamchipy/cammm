8809d4b4deb9ef85f62ff3cde36d9d47fc6c4cb0
Chipy <iamchipy@gmail.com>
2023-11-13 16:20:47 -0500

    removing bat

--- cammm_raw_UPLOAD.bat
+++
@@ -1,4 +0,0 @@
-
-REM # Upload the files in question
-"C:\Program Files (x86)\WinSCP\winscp.com" /command "open ftp://upload@chipy.dev:1shortDUMBstring!@chipy.dev/" "put cammm_raw.csv /" "exit"
-pause

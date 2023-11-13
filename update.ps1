094d456d865a48b7e4ff00428685196949fc1a4c
Chipy <iamchipy@gmail.com>
2023-11-13 16:25:10 -0500

    clean up

--- update.ps1
+++
@@ -1,6 +0,0 @@
-Stop-Process -Name "cammm"
-if(Test-Path -Path "C:\Dropbox\_SCRIPTS\cammm\cammm.old") {Remove-Item "C:\Dropbox\_SCRIPTS\cammm\cammm.old"}
-Rename-Item -Path "C:\Dropbox\_SCRIPTS\cammm\cammm.exe" -NewName "C:\Dropbox\_SCRIPTS\cammm\cammm.old" -force
-if(Test-Path -Path "C:\Dropbox\_SCRIPTS\cammm\cammm.exe"){ Remove-Item "C:\Dropbox\_SCRIPTS\cammm\cammm.exe"}
-Rename-Item -Path "C:\Dropbox\_SCRIPTS\cammm\cammm.new" -NewName "C:\Dropbox\_SCRIPTS\cammm\cammm.exe" -force
-& "C:\Dropbox\_SCRIPTS\cammm\cammm.exe"
